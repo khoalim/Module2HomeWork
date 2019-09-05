ecb4e938e499fecaf308bb14173c657c376f0846
Kim Hoalim <kthoalim@gmail.com>
2019-09-05 01:47:54 -0400

    Initial Commit

---
+++ Module2HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Module2HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+
+            Console.ReadLine();
+
+            
+
+        
+        }
+    }
+}
